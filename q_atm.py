7c39550c5e118f78a4e17e1c707d8aa649ea181b
bruno <bruno.crippa@unimi.it>
2025-03-06 16:05:36 +0100

    First commit ATM_query

---
+++ q_atm.py
@@ -0,0 +1,61 @@
+#!python
+import urllib.parse
+import requests
+headers={
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0"
+            },
+            {
+              "name": "Accept",
+              "value": "image/avif,image/webp,image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3"
+            },
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, br, zstd"
+            },
+            {
+              "name": "DNT",
+              "value": "1"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "Referer",
+              "value": "https://giromilano.atm.it/"
+            },
+            {
+              "name": "Sec-Fetch-Dest",
+              "value": "image"
+            },
+            {
+              "name": "Sec-Fetch-Mode",
+              "value": "cors"
+            },
+            {
+              "name": "Sec-Fetch-Site",
+              "value": "same-origin"
+            }
+          }
+
+
+url='https://giromilano.atm.it/proxy.tpportal/api/tpPortal/geodata/pois/stops/12660'
+
+# Step 1: Make an HTTP request to the API endpoint
+response = requests.get(url)#,headers=headers)
+print(response)
+# Step 2: Parse the JSON response
+#data = response.json()
+
+# Step 3: Extract specific data
+#CustomerCade = data['CustomerCode']
+#WaitMessage = data['Lines'][0]['WaitMessage']
+
+# Step 4: Print the extracted data
+#print(f"CustomerCode: {CustomerCode}")
+#print(f"WaitMessage: {WaitMessage}")
---
+++ q_atm_param.py
@@ -0,0 +1,52 @@
+import sys
+import requests
+
+cookies = {
+    '_ga': 'GA1.1.2037073293.1738697629',
+    '_gid': 'GA1.1.1857438754.1738697629',
+    '_ga': 'GA1.1.2037073293.1738697629',
+    '_ga_RD7BG8RLV0': 'GS1.1.1738697628.1.0.1738697628.0.0.0',
+    '_gat': '1',
+}
+
+headers = {
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+    # 'cookie': '_ga=GA1.1.2037073293.1738697629; _gid=GA1.1.1857438754.1738697629; _ga=GA1.1.2037073293.1738697629; _ga_RD7BG8RLV0=GS1.1.1738697628.1.0.1738697628.0.0.0; _gat=1',
+    'dnt': '1',
+    'priority': 'u=1, i',
+    'referer': 'https://giromilano.atm.it/',
+    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+}
+
+print(sys.argv)
+linea=sys.argv[1]
+response = requests.get(
+    'https://giromilano.atm.it/proxy.tpportal/api/tpPortal/geodata/pois/stops/'+linea,
+    cookies=cookies,
+    headers=headers,
+)
+print(response)
+
+
+
+data = response.json()
+
+# Step 3: Extract specific data
+CustomerCode = data['CustomerCode']
+WaitMessage = data['Lines'][0]['WaitMessage']
+Address = data['Address']
+LineDescription = data['Lines'][0]['Line']['LineDescription']
+LineCode = data['Lines'][0]['Line']['LineCode']
+# Step 4: Print the extracted data
+print(f"CustomerCode: {CustomerCode}")
+print(f"WaitMessage: {WaitMessage}")
+print(f"Address: {Address}")
+print(f"LineDescription: {LineDescription}")
+print(f"Line: {LineCode}")
